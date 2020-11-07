2d50a9ed9960bdb75865c757b4664263f186feb7
Zuucker <kamigie504@student.polsl.pl>
2020-11-07 17:20:10 +0100

    handling of diffrent modes

--- main/main.cpp
+++ main/main.cpp
@@ -55,5 +55,59 @@ int main(int argc, char* argv[])
         }
     }
 
+
+
+    if (inputName == "")
+    {
+        if (argc != 1 && h == false)
+        {
+            cout << endl;
+            cout << "Please provide the needed files!" << endl;
+            help();
+        }
+    }
+    else {
+        if (operation == 1)
+        {
+            if (keyName == "" || outputName == "")
+            {
+                cout << endl;
+                cout << "Please provide the needed files!" << endl;
+                help();
+                exit(0);
+            }
+            string key = readFile(keyName);
+            string input = readFile(inputName);
+            string output = encrypting(prepareKey(key, input), input);
+            writeFile(outputName, output);
+            cout << "Encrypted!" << endl;
+        }
+        else if (operation == 2)
+        {
+            if (keyName == "" || outputName == "")
+            {
+                cout << endl;
+                cout << "Please provide the needed files!" << endl;
+                help();
+                exit(0);
+            }
+            string key = readFile(keyName);
+            string input = readFile(inputName);
+            string output = decrypting(prepareKey(key, input), input);
+            writeFile(outputName, output);
+            cout << "Decrypted!" << endl;
+        }
+        else if (operation == 3)
+        {
+            
+
+        }
+        else if (operation == 0)
+        {
+            cout << "Please provide a correct flag!";
+            help();
+        }
+    }
+
 	return 0;
 }
