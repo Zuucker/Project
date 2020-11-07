5fa3a59842ab2f066541b9512cef48de9168aef9
Zuucker <kamigie504@student.polsl.pl>
2020-11-07 16:41:46 +0100

    a matrix filling function

--- main/header.h
+++ main/header.h
@@ -1,3 +1,29 @@
 #pragma once
+#include <iostream>
+#include <array>
+
 
 using namespace std;
+
+extern array<array<char, 26>, 26> matryca;
+
+void fill(array<array<char, 26>, 26>& mat) {
+
+    int k = 0;
+    char nast = 'a';
+    for (int i = 0; i < 26; i++)
+    {
+        for (char j = nast; k < 26; j++)
+        {
+            mat[i][k] = j;
+            if (j == 'z')
+            {
+                j = '`';
+            }
+            k++;
+        }
+        k = 0;
+
+        nast++;
+    }
+}
